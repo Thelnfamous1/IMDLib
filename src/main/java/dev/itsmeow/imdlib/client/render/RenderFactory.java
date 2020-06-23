ec0e2cb9ced92c5e1c9de934fc206f778ce6589e
itsmeow <mods.itsmeow@gmail.com>
2020-06-23 19:42:50 -0400

    Add render factories

---
+++ src/main/java/dev/itsmeow/imdlib/client/render/RenderFactory.java
@@ -0,0 +1,49 @@
+package dev.itsmeow.imdlib.client.render;
+
+import java.util.function.Function;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.IRendersAsItem;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+
+public class RenderFactory {
+
+    public final String modid;
+
+    public RenderFactory(String modid) {
+        this.modid = modid;
+    }
+
+    public <T extends MobEntity, M extends EntityModel<T>> ImplRenderer.Builder<T, M> r(float shadowSize) {
+        return ImplRenderer.<T, M>factory(modid, shadowSize);
+    }
+
+    public <T extends MobEntity, M extends EntityModel<T>> void addRender(Class<T> clazz, float shadowSize, Function<ImplRenderer.Builder<T, M>, ImplRenderer.Builder<T, M>> render) {
+        RenderingRegistry.registerEntityRenderingHandler(clazz, render.apply(this.<T, M>r(shadowSize)).done());
+    }
+
+    public static <T extends Entity> void addRender(Class<T> clazz, IRenderFactory<T> renderer) {
+        RenderingRegistry.registerEntityRenderingHandler(clazz, renderer);
+    }
+
+    public static <T extends Entity & IRendersAsItem> IRenderFactory<T> sprite() {
+        return mgr -> new SpriteRenderer<T>(mgr, Minecraft.getInstance().getItemRenderer());
+    }
+
+    public static <T extends Entity> IRenderFactory<T> nothing() {
+        return r -> new EntityRenderer<T>(r) {
+            @Override
+            protected ResourceLocation getEntityTexture(T entity) {
+                return null;
+            }
+        };
+    }
+
+}
