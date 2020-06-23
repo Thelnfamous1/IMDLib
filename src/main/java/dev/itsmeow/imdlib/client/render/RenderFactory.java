c55d6c7b3450773058604677649b6967fd86b1a8
itsmeow <mods.itsmeow@gmail.com>
2020-06-23 19:45:20 -0400

    Render Factory 1.15

--- src/main/java/dev/itsmeow/imdlib/client/render/RenderFactory.java
+++ src/main/java/dev/itsmeow/imdlib/client/render/RenderFactory.java
@@ -7,6 +7,7 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.IRendersAsItem;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.util.ResourceLocation;
@@ -25,11 +26,11 @@ public class RenderFactory {
         return ImplRenderer.<T, M>factory(modid, shadowSize);
     }
 
-    public <T extends MobEntity, M extends EntityModel<T>> void addRender(Class<T> clazz, float shadowSize, Function<ImplRenderer.Builder<T, M>, ImplRenderer.Builder<T, M>> render) {
+    public <T extends MobEntity, M extends EntityModel<T>> void addRender(EntityType<T> clazz, float shadowSize, Function<ImplRenderer.Builder<T, M>, ImplRenderer.Builder<T, M>> render) {
         RenderingRegistry.registerEntityRenderingHandler(clazz, render.apply(this.<T, M>r(shadowSize)).done());
     }
 
-    public static <T extends Entity> void addRender(Class<T> clazz, IRenderFactory<T> renderer) {
+    public static <T extends Entity> void addRender(EntityType<T> clazz, IRenderFactory<T> renderer) {
         RenderingRegistry.registerEntityRenderingHandler(clazz, renderer);
     }
 
@@ -40,7 +41,7 @@ public class RenderFactory {
     public static <T extends Entity> IRenderFactory<T> nothing() {
         return r -> new EntityRenderer<T>(r) {
             @Override
-            protected ResourceLocation getEntityTexture(T entity) {
+            public ResourceLocation getEntityTexture(T entity) {
                 return null;
             }
         };
