5c38e4b57d2074f2b501d66fe35d021497d99765
itsmeow <mods.itsmeow@gmail.com>
2020-09-05 18:29:12 -0400

    Add data generators to egg

--- src/main/java/dev/itsmeow/imdlib/item/ModSpawnEggItem.java
+++ src/main/java/dev/itsmeow/imdlib/item/ModSpawnEggItem.java
@@ -1,24 +1,31 @@
 package dev.itsmeow.imdlib.item;
 
 import dev.itsmeow.imdlib.entity.util.EntityTypeContainer;
+import net.minecraft.data.DataGenerator;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
 public class ModSpawnEggItem extends SpawnEggItem {
 
     private final EntityType<?> type;
     private final String modid;
+    private final String name;
 
     public ModSpawnEggItem(EntityTypeContainer<?> container) {
         super(container.entityType, container.eggColorSolid, container.eggColorSpot, new Properties().group(ItemGroup.MISC));
         this.type = container.entityType;
         this.modid = container.getModId();
-        this.setRegistryName(container.getModId(), container.entityName.toLowerCase().toString() + "_spawn_egg");
+        this.name = container.entityName.toLowerCase();
+        this.setRegistryName(container.getModId(), name + "_spawn_egg");
     }
 
     @Override
@@ -45,4 +52,28 @@ public class ModSpawnEggItem extends SpawnEggItem {
         return type == this.type;
     }
 
+    public DataProvider getDataProvider(GatherDataEvent event) {
+        return new DataProvider(this, event.getGenerator(), event.getExistingFileHelper());
+    }
+
+    public static class DataProvider extends ItemModelProvider {
+
+        private String name;
+
+        public DataProvider(ModSpawnEggItem i, DataGenerator generator, ExistingFileHelper existingFileHelper) {
+            super(generator, i.modid, existingFileHelper);
+            this.name = i.name;
+        }
+
+        @Override
+        public String getName() {
+            return name + "_spawn_egg";
+        }
+
+        @Override
+        protected void registerModels() {
+            this.withExistingParent(name + "_spawn_egg", new ResourceLocation("minecraft", "item/template_spawn_egg"));
+        }
+
+    }
 }
