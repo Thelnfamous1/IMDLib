189734287dd4cd21a74d799c0d70094139ad1bc5
itsmeow <mods.itsmeow@gmail.com>
2020-09-12 14:04:25 -0400

    Fix selective variants not applying to baby animals when spawned as babies

--- src/main/java/dev/itsmeow/imdlib/entity/util/ISelectiveVariantTypes.java
+++ src/main/java/dev/itsmeow/imdlib/entity/util/ISelectiveVariantTypes.java
@@ -19,30 +19,28 @@ public interface ISelectiveVariantTypes<T extends MobEntity> extends IVariantTyp
     @Nullable
     @Override
     default ILivingEntityData initData(IWorld world, SpawnReason reason, ILivingEntityData livingdata) {
-        if(!this.getImplementation().isChild()) {
-            if(this.getContainer().biomeVariants && (reason == SpawnReason.CHUNK_GENERATION || reason == SpawnReason.NATURAL)) {
-                Biome biome = world.getBiome(this.getImplementation().getPosition());
-                String[] validTypes = this.getTypesFor(biome, BiomeDictionary.getTypes(RegistryKey.getOrCreateKey(Keys.BIOMES, biome.getRegistryName())));
-                String varStr = validTypes[this.getImplementation().getRNG().nextInt(validTypes.length)];
-                IVariant variant = this.getContainer().getVariantForName(varStr);
-                if(variant == null || !varStr.equals(variant.getName())) {
-                    throw new RuntimeException("Received invalid variant string from selective type: " + varStr + " on entity " + this.getContainer().entityName);
-                }
-                if(livingdata instanceof TypeData) {
-                    variant = ((TypeData) livingdata).typeData;
-                } else {
-                    livingdata = new TypeData(variant);
-                }
-                this.setType(variant);
-            } else {
-                IVariant variant = this.getRandomType();
-                if(livingdata instanceof TypeData) {
-                    variant = ((TypeData) livingdata).typeData;
-                } else {
-                    livingdata = new TypeData(variant);
-                }
-                this.setType(variant);
+        if(this.getContainer().biomeVariants && (reason == SpawnReason.CHUNK_GENERATION || reason == SpawnReason.NATURAL)) {
+            Biome biome = world.getBiome(this.getImplementation().getPosition());
+            String[] validTypes = this.getTypesFor(biome, BiomeDictionary.getTypes(RegistryKey.getOrCreateKey(Keys.BIOMES, biome.getRegistryName())));
+            String varStr = validTypes[this.getImplementation().getRNG().nextInt(validTypes.length)];
+            IVariant variant = this.getContainer().getVariantForName(varStr);
+            if(variant == null || !varStr.equals(variant.getName())) {
+                throw new RuntimeException("Received invalid variant string from selective type: " + varStr + " on entity " + this.getContainer().entityName);
             }
+            if(livingdata instanceof TypeData) {
+                variant = ((TypeData) livingdata).typeData;
+            } else {
+                livingdata = new TypeData(variant);
+            }
+            this.setType(variant);
+        } else {
+            IVariant variant = this.getRandomType();
+            if(livingdata instanceof TypeData) {
+                variant = ((TypeData) livingdata).typeData;
+            } else {
+                livingdata = new TypeData(variant);
+            }
+            this.setType(variant);
         }
         return livingdata;
     }
