c8fc953cca5f1b5cd84c42b261cfd90429bea481
itsmeow <mods.itsmeow@gmail.com>
2020-10-08 06:17:11 -0400

    Port containability system from BAP

---
+++ src/main/java/dev/itsmeow/imdlib/entity/util/EntityTypeContainerContainable.java
@@ -0,0 +1,129 @@
+package dev.itsmeow.imdlib.entity.util;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+import dev.itsmeow.imdlib.entity.util.builder.AbstractEntityBuilder;
+import dev.itsmeow.imdlib.entity.util.builder.EntityTypeDefinition;
+import dev.itsmeow.imdlib.item.IContainerItem;
+import dev.itsmeow.imdlib.item.IContainerItem.ITooltipFunction;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.item.Item;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.world.World;
+
+public class EntityTypeContainerContainable<T extends MobEntity & IContainable, I extends Item & IContainerItem<T>> extends EntityTypeContainer<T> {
+
+    protected DataParameter<Boolean> fromContainerDataKey;
+    protected I containerItem;
+    protected Item emptyContainerItem;
+
+    protected EntityTypeContainerContainable(ContainableEntityTypeDefinition<T, I, EntityTypeContainerContainable<T, I>> def) {
+        super(def);
+        this.containerItem = def.getContainerSupplier().apply(this, def.getTooltipFunction());
+        this.emptyContainerItem = def.getEmptyContainerSupplier().apply(this);
+    }
+
+    protected static class ContainableEntityTypeDefinition<T extends MobEntity & IContainable, I extends Item & IContainerItem<T>, C extends EntityTypeContainerContainable<T, I>> extends EntityTypeDefinition<T> {
+        AbstractEntityBuilderContainable<T, I, C, ?> builder;
+
+        public ContainableEntityTypeDefinition(AbstractEntityBuilderContainable<T, I, C, ?> builder) {
+            super(builder);
+            this.builder = builder;
+        }
+
+        public ITooltipFunction getTooltipFunction() {
+            return builder.tooltipFinal;
+        }
+
+        public BiFunction<C, ITooltipFunction, I> getContainerSupplier() {
+            return builder.containerSupplier;
+        }
+
+        public Function<C, Item> getEmptyContainerSupplier() {
+            return builder.emptyContainerSupplier;
+        }
+
+    }
+
+    public static abstract class AbstractEntityBuilderContainable<T extends MobEntity & IContainable, I extends Item & IContainerItem<T>, C extends EntityTypeContainerContainable<T, I>, B extends AbstractEntityBuilderContainable<T, I, C, B>> extends AbstractEntityBuilder<T, C, B> {
+
+        protected ITooltipFunction tooltip;
+        protected ITooltipFunction tooltipFinal;
+        protected BiFunction<C, ITooltipFunction, I> containerSupplier;
+        protected Function<C, Item> emptyContainerSupplier;
+
+        protected AbstractEntityBuilderContainable(Class<T> EntityClass, Function<World, T> func, String entityNameIn, String modid) {
+            super(EntityClass, func, entityNameIn, modid);
+        }
+
+        public B containers(BiFunction<C, ITooltipFunction, I> containerSupplier, Function<C, Item> emptyContainerSupplier) {
+            return containers(containerSupplier, emptyContainerSupplier, null);
+        }
+
+        public B containers(BiFunction<C, ITooltipFunction, I> containerSupplier, Function<C, Item> emptyContainerSupplier, ITooltipFunction tooltip) {
+            this.containerSupplier = containerSupplier;
+            this.emptyContainerSupplier = emptyContainerSupplier;
+            this.tooltip = tooltip;
+            return getImplementation();
+        }
+
+        @Override
+        public void preBuild() {
+            if(variantCount > 0) {
+                if(this.tooltip == null) {
+                    this.tooltipFinal = IContainerItem.VARIANT_TOOLTIP;
+                } else {
+                    this.tooltipFinal = (container, stack, worldIn, tooltip) -> {
+                        IContainerItem.VARIANT_TOOLTIP.addInformation(container, stack, worldIn, tooltip);
+                        this.tooltip.addInformation(container, stack, worldIn, tooltip);
+                    };
+                }
+            } else if(this.tooltip != null) {
+                this.tooltipFinal = this.tooltip;
+            } else if(this.tooltip == null) {
+                this.tooltipFinal = (container, stack, world, tooltip) -> {
+                };
+            }
+        }
+    }
+
+    public static class Builder<T extends MobEntity & IContainable, I extends Item & IContainerItem<T>> extends AbstractEntityBuilderContainable<T, I, EntityTypeContainerContainable<T, I>, Builder<T, I>> {
+
+        protected Builder(Class<T> EntityClass, Function<World, T> func, String entityNameIn, String modid) {
+            super(EntityClass, func, entityNameIn, modid);
+        }
+
+        @Override
+        public EntityTypeContainerContainable<T, I> rawBuild() {
+            return new EntityTypeContainerContainable<T, I>(new ContainableEntityTypeDefinition<T, I, EntityTypeContainerContainable<T, I>>(this));
+        }
+
+        @Override
+        public Builder<T, I> getImplementation() {
+            return this;
+        }
+
+        public static <T extends MobEntity & IContainable, I extends Item & IContainerItem<T>> Builder<T, I> create(Class<T> EntityClass, Function<World, T> func, String entityNameIn, String modid) {
+            return new Builder<T, I>(EntityClass, func, entityNameIn, modid);
+        }
+    }
+
+    public DataParameter<Boolean> getFromContainerDataKey() {
+        if(this.fromContainerDataKey == null) {
+            this.fromContainerDataKey = EntityDataManager.<Boolean>createKey(this.entityClass, DataSerializers.BOOLEAN);
+        }
+        return this.fromContainerDataKey;
+    }
+
+    public I getContainerItem() {
+        return this.containerItem;
+    }
+
+    public Item getEmptyContainerItem() {
+        return this.emptyContainerItem;
+    }
+
+}
---
+++ src/main/java/dev/itsmeow/imdlib/item/ItemModFishBucket.java
@@ -0,0 +1,75 @@
+package dev.itsmeow.imdlib.item;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+import javax.annotation.Nullable;
+
+import dev.itsmeow.imdlib.entity.util.EntityTypeContainer;
+import dev.itsmeow.imdlib.entity.util.EntityTypeContainerContainable;
+import dev.itsmeow.imdlib.entity.util.IContainable;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BucketItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class ItemModFishBucket<T extends MobEntity & IContainable> extends BucketItem implements IContainerItem<T> {
+    private final EntityTypeContainerContainable<T, ItemModFishBucket<T>> typeContainer;
+    private ITooltipFunction tooltip;
+
+    public static final <T extends MobEntity & IContainable> BiFunction<EntityTypeContainerContainable<T, ItemModFishBucket<T>>, ITooltipFunction, ItemModFishBucket<T>> waterBucket(ItemGroup group) {
+        return (container, tooltip) -> {
+            return new ItemModFishBucket<T>(container, () -> Fluids.WATER, tooltip, group);
+        };
+    }
+
+    public ItemModFishBucket(EntityTypeContainerContainable<T, ItemModFishBucket<T>> typeContainer, Supplier<? extends Fluid> fluid, ItemGroup group) {
+        this(typeContainer, fluid, IContainerItem.VARIANT_TOOLTIP, group);
+    }
+
+    public ItemModFishBucket(EntityTypeContainerContainable<T, ItemModFishBucket<T>> typeContainer, Supplier<? extends Fluid> fluid, ITooltipFunction tooltip, ItemGroup group) {
+        super(fluid, new Item.Properties().maxStackSize(1).group(group));
+        this.addPropertyOverrides(this);
+        this.typeContainer = typeContainer;
+        this.setRegistryName(typeContainer.entityName + "_bucket");
+        this.tooltip = tooltip;
+    }
+
+    @Override
+    public void onLiquidPlaced(World worldIn, ItemStack stack, BlockPos pos) {
+        if(!worldIn.isRemote) {
+            this.placeEntity(worldIn, stack, pos);
+        }
+    }
+
+    @Override
+    protected void playEmptySound(@Nullable PlayerEntity player, IWorld worldIn, BlockPos pos) {
+        worldIn.playSound(player, pos, SoundEvents.ITEM_BUCKET_EMPTY_FISH, SoundCategory.NEUTRAL, 1.0F, 1.0F);
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+        this.tooltip.addInformation(this.typeContainer, stack, worldIn, tooltip);
+    }
+
+    @Override
+    public EntityTypeContainer<T> getContainer() {
+        return typeContainer;
+    }
+
+}
