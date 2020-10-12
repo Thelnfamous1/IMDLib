d1cca4416533f9d8bc61602484e72c47d790c190
itsmeow <mods.itsmeow@gmail.com>
2020-10-11 23:40:10 -0400

    Merge 1.16

--- src/main/java/dev/itsmeow/imdlib/item/IContainerItem.java
+++ src/main/java/dev/itsmeow/imdlib/item/IContainerItem.java
@@ -30,7 +30,7 @@ public interface IContainerItem<T extends MobEntity & IContainable> {
         if(compoundnbt != null && compoundnbt.contains("BucketVariantTag", Constants.NBT.TAG_STRING)) {
             String id = compoundnbt.getString("BucketVariantTag");
             TextFormatting[] atextformatting = new TextFormatting[] { TextFormatting.ITALIC, TextFormatting.GRAY };
-            tooltip.add((new TranslationTextComponent("entity.betteranimalsplus." + container.entityName.toLowerCase() + ".type." + container.getVariantForName(id).getName())).applyTextStyles(atextformatting));
+            tooltip.add((new TranslationTextComponent("entity." + container.getModId() + "." + container.entityName.toLowerCase() + ".type." + container.getVariantForName(id).getName())).applyTextStyles(atextformatting));
         }
     };
 
