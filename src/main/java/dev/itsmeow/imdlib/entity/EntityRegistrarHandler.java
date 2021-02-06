9d0db358a98f80c8b8ddc2b1692a9506b2cf611d
itsmeow <mods.itsmeow@gmail.com>
2021-02-05 21:41:02 -0500

    Fix biome loading and configuration not applying issues

--- src/main/java/dev/itsmeow/imdlib/entity/EntityRegistrarHandler.java
+++ src/main/java/dev/itsmeow/imdlib/entity/EntityRegistrarHandler.java
@@ -1,12 +1,5 @@
 package dev.itsmeow.imdlib.entity;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.LinkedHashMap;
-import java.util.function.Function;
-
-import org.apache.logging.log4j.LogManager;
-
 import dev.itsmeow.imdlib.entity.util.EntityTypeContainer;
 import dev.itsmeow.imdlib.entity.util.builder.IEntityBuilder;
 import dev.itsmeow.imdlib.item.ModSpawnEggItem;
@@ -15,18 +8,32 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.MutableRegistry;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.MobSpawnInfo;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.common.thread.SidedThreadGroups;
+import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import org.apache.logging.log4j.LogManager;
+
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
+import java.util.function.Function;
 
 public class EntityRegistrarHandler {
 
     public final String modid;
     public final LinkedHashMap<String, EntityTypeContainer<? extends MobEntity>> ENTITIES = new LinkedHashMap<>();
     private static final Field SERIALIZABLE = ObfuscationReflectionHelper.findField(EntityType.class, "field_200733_aL");
+    private static Map<RegistryKey<Biome>, WeakReference<MobSpawnInfo>> spawnInfo = new HashMap<>();
 
 
     public EntityRegistrarHandler(String modid) {
@@ -103,20 +110,39 @@ public class EntityRegistrarHandler {
         public void onLoad() {
             // Replace entity data
             ENTITIES.values().forEach(EntityTypeContainer::configurationLoad);
-        }
 
-        /**
-         * Make sure the event subscriber for this is set to HIGH priority!
-         */
-        public void biomeLoad(BiomeLoadingEvent event) {
-            // Add spawns based on new container data
-            if(!ENTITIES.values().isEmpty()) {
-                for(EntityTypeContainer<?> entry : ENTITIES.values()) {
-                    if(entry.doSpawning && entry.spawnWeight > 0 && entry.getBiomeIDs().contains(event.getName().toString())) {
-                        entry.registerPlacement();
-                        MobSpawnInfo.Builder b = event.getSpawns().withSpawner(entry.spawnType, entry.getSpawnEntry());
-                        if(entry.spawnCostPer != 0 && entry.spawnMaxCost != 0) {
-                            b.withSpawnCost(entry.entityType, entry.spawnCostPer, entry.spawnMaxCost);
+            if(Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER) {
+                MutableRegistry<Biome> biomeRegistry = ServerLifecycleHooks.getCurrentServer().func_244267_aX().getRegistry(Registry.BIOME_KEY);
+                for(ResourceLocation key : biomeRegistry.keySet()) {
+                    Biome biome = biomeRegistry.getOptional(key).get();
+                    MobSpawnInfo spawnInfo = biome.getMobSpawnInfo();
+                    // make spawns mutable
+                    spawnInfo.spawners = new HashMap<>(spawnInfo.spawners);
+                    // make spawner lists mutable
+                    for(EntityClassification classification : EntityClassification.values()) {
+                        ArrayList<MobSpawnInfo.Spawners> newList = new ArrayList<>();
+                        List<MobSpawnInfo.Spawners> oldList = spawnInfo.spawners.get(classification);
+                        if(oldList != null) {
+                            for (MobSpawnInfo.Spawners spawner : oldList) {
+                                newList.add(spawner);
+                            }
+                        }
+                        spawnInfo.spawners.put(classification, newList);
+                    }
+                    // make costs mutable
+                    spawnInfo.spawnCosts = new HashMap<>(spawnInfo.spawnCosts);
+                    for(EntityTypeContainer<?> entry : ENTITIES.values()) {
+                        if(entry.doSpawning && entry.spawnWeight > 0 && entry.getBiomeIDs().contains(key.toString())) {
+                            entry.registerPlacement();
+                            List<MobSpawnInfo.Spawners> list = spawnInfo.spawners.get(entry.spawnType);
+                            if (list != null) {
+                                list.add(entry.getSpawnEntry());
+                            }
+                            if (entry.spawnCostPer != 0 && entry.spawnMaxCost != 0) {
+                                // stupid private constructors
+                                MobSpawnInfo.SpawnCosts costs = new MobSpawnInfo.Builder().withSpawnCost(entry.entityType, entry.spawnCostPer, entry.spawnMaxCost).copy().spawnCosts.get(entry.entityType);
+                                spawnInfo.spawnCosts.put(entry.entityType, costs);
+                            }
                         }
                     }
                 }
