2e7a0c122fefdd287f7a3716bb0ecf57af2bc1e3
ffrann <45723631+frqnny@users.noreply.github.com>
2021-05-06 10:03:06 -0400

    Basic skeleton of the mod

---
+++ fabric/src/main/java/dev/itsmeow/betteranimalsplus/FabricMod.java
@@ -0,0 +1,10 @@
+package dev.itsmeow.betteranimalsplus;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        BetterAnimalsPlus.init();
+    }
+}
