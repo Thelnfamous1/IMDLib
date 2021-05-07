84314fd1d1798ea7f045b7e2ce5d70962c4a1efe
ffrann <45723631+frqnny@users.noreply.github.com>
2021-05-07 09:58:03 -0400

    Biome types impl added

--- fabric/src/main/java/dev/itsmeow/betteranimalsplus/fabric/BiomeTypesImpl.java
+++ fabric/src/main/java/dev/itsmeow/betteranimalsplus/fabric/BiomeTypesImpl.java
@@ -8,7 +8,54 @@ import net.minecraft.world.level.biome.Biome;
 public class BiomeTypesImpl {
 
     public static void init() {
-        BiomeTypes.FOREST = new BiomeTypes.Type((biome) -> IMDLib.REGISTRIES.get().get(Registry.BIOME_REGISTRY).get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.FOREST);
+        //otherwise it was getting the vanilla registry but .get gets the architectury registry
+        me.shedaniel.architectury.registry.Registry<Biome> reg = IMDLib.REGISTRIES.get().get(Registry.BIOME_REGISTRY);
 
+        BiomeTypes.HOT = new BiomeTypes.Type(biome -> {
+            Biome biomeIn = reg.get(biome.location());
+            float temperature = biomeIn.getBaseTemperature();
+            Biome.BiomeCategory category = biomeIn.getBiomeCategory();
+            return temperature > 0.75 || category == Biome.BiomeCategory.DESERT;
+        });
+        BiomeTypes.COLD = new BiomeTypes.Type(biome -> {
+            Biome biomeIn = reg.get(biome.location());
+            float temperature = biomeIn.getBaseTemperature();
+            Biome.BiomeCategory category = biomeIn.getBiomeCategory();
+            return temperature < 0.1F || category == Biome.BiomeCategory.ICY;
+        });
+        BiomeTypes.SPARSE = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.DENSE = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.WET = new BiomeTypes.Type(biome -> reg.get(biome.location()).isHumid());
+        BiomeTypes.DRY = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.DESERT);
+        BiomeTypes.SAVANNA = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.SAVANNA);
+        BiomeTypes.CONIFEROUS = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.TAIGA);
+        BiomeTypes.JUNGLE = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.JUNGLE);
+        BiomeTypes.SPOOKY = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.DEAD = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.LUSH = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.MUSHROOM = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.MUSHROOM);
+        BiomeTypes.MAGICAL = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.RARE = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.PLATEAU = new BiomeTypes.Type((biome) -> biome.location().getPath().contains("plateau"));
+        BiomeTypes.MODIFIED = new BiomeTypes.Type((biome) -> biome.location().getPath().contains("modified"));
+        BiomeTypes.OCEAN = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.OCEAN);
+        BiomeTypes.RIVER = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.RIVER);
+        BiomeTypes.WATER = new BiomeTypes.Type((biome) -> {
+            Biome.BiomeCategory category = reg.get(biome.location()).getBiomeCategory();
+            return category == Biome.BiomeCategory.OCEAN || category == Biome.BiomeCategory.RIVER;
+        });
+        BiomeTypes.MESA = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.MESA);
+        BiomeTypes.FOREST = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.FOREST);
+        BiomeTypes.PLAINS = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.PLAINS);
+        BiomeTypes.MOUNTAIN = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.EXTREME_HILLS);
+        BiomeTypes.HILL = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.EXTREME_HILLS);
+        BiomeTypes.SWAMP = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.SWAMP);
+        BiomeTypes.SANDY = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.DESERT);
+        BiomeTypes.SNOWY = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.ICY);
+        BiomeTypes.WASTELAND = new BiomeTypes.Type(biome -> false);
+        BiomeTypes.BEACH = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.BEACH);
+        BiomeTypes.VOID = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.NONE);
+        BiomeTypes.NETHER = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.NETHER);
+        BiomeTypes.END = new BiomeTypes.Type((biome) -> reg.get(biome.location()).getBiomeCategory() == Biome.BiomeCategory.THEEND);
     }
 }
--- forge/src/main/java/dev/itsmeow/betteranimalsplus/forge/BiomeTypesImpl.java
+++ forge/src/main/java/dev/itsmeow/betteranimalsplus/forge/BiomeTypesImpl.java
@@ -6,7 +6,39 @@ import net.minecraftforge.common.BiomeDictionary;
 public class BiomeTypesImpl {
 
     public static void init() {
-        BiomeTypes.FOREST = new BiomeTypes.Type((biome) -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.FOREST));
+        BiomeTypes.HOT = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.HOT));
+        BiomeTypes.COLD = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.COLD));
+        BiomeTypes.SPARSE = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.SPARSE));
+        BiomeTypes.DENSE = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.DENSE));
+        BiomeTypes.WET = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.WET));
+        BiomeTypes.DRY = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.DRY));
+        BiomeTypes.SAVANNA = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.SAVANNA));
+        BiomeTypes.CONIFEROUS = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.CONIFEROUS));
+        BiomeTypes.JUNGLE = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.JUNGLE));
+        BiomeTypes.SPOOKY = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.SPOOKY));
+        BiomeTypes.DEAD = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.DEAD));
+        BiomeTypes.LUSH = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.LUSH));
+        BiomeTypes.MUSHROOM = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.MUSHROOM));
+        BiomeTypes.MAGICAL = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.MAGICAL));
+        BiomeTypes.RARE = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.RARE));
+        BiomeTypes.PLATEAU = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.PLATEAU));
+        BiomeTypes.MODIFIED = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.MODIFIED));
+        BiomeTypes.OCEAN = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.OCEAN));
+        BiomeTypes.RIVER = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.RIVER));
+        BiomeTypes.WATER = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.WATER));
+        BiomeTypes.MESA = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.MESA));
+        BiomeTypes.FOREST = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.FOREST));
+        BiomeTypes.PLAINS = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.PLAINS));
+        BiomeTypes.MOUNTAIN = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.MOUNTAIN));
+        BiomeTypes.HILL = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.HILLS));
+        BiomeTypes.SWAMP = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.SWAMP));
+        BiomeTypes.SANDY = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.SANDY));
+        BiomeTypes.SNOWY = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.SNOWY));
+        BiomeTypes.WASTELAND = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.WASTELAND));
+        BiomeTypes.BEACH = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.BEACH));
+        BiomeTypes.VOID = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.VOID));
+        BiomeTypes.NETHER = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.NETHER));
+        BiomeTypes.END = new BiomeTypes.Type(biome -> BiomeDictionary.hasType(biome, BiomeDictionary.Type.END));
 
     }
 }
