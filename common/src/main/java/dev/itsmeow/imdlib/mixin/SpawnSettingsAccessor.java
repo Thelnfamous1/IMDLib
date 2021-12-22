0a380c48e0c8a385008063e66da4dfe61faa1af7
itsmeow <itsmeow@itsmeow.dev>
2021-12-22 06:57:13 -0500

    Add @Mutable to accessor set methods

--- common/src/main/java/dev/itsmeow/imdlib/mixin/SpawnSettingsAccessor.java
+++ common/src/main/java/dev/itsmeow/imdlib/mixin/SpawnSettingsAccessor.java
@@ -4,6 +4,7 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
@@ -15,12 +16,14 @@ public interface SpawnSettingsAccessor {
     @Accessor("spawners")
     Map<MobCategory, List<MobSpawnSettings.SpawnerData>> getSpawners();
 
+    @Mutable
     @Accessor("spawners")
     void setSpawners(Map<MobCategory, List<MobSpawnSettings.SpawnerData>> spawners);
 
     @Accessor("mobSpawnCosts")
     Map<EntityType<?>, MobSpawnSettings.MobSpawnCost> getMobSpawnCosts();
 
+    @Mutable
     @Accessor("mobSpawnCosts")
     void setMobSpawnCosts(Map<EntityType<?>, MobSpawnSettings.MobSpawnCost> costs);
 }
