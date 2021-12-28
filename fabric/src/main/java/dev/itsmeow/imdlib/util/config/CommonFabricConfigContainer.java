e1d6d9060b3eb06c772e55b88a46e1da5138e718
itsmeow <itsmeow@itsmeow.dev>
2021-12-28 00:58:03 -0500

    Fix client config not initializing when connecting to server

--- fabric/src/main/java/dev/itsmeow/imdlib/util/config/CommonFabricConfigContainer.java
+++ fabric/src/main/java/dev/itsmeow/imdlib/util/config/CommonFabricConfigContainer.java
@@ -1,6 +1,10 @@
 package dev.itsmeow.imdlib.util.config;
 
 import dev.architectury.event.events.common.LifecycleEvent;
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
+import dev.itsmeow.imdlib.util.ClassLoadHacks;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.minecraft.util.LazyLoadedValue;
 
 import java.util.HashSet;
@@ -16,6 +20,10 @@ public class CommonFabricConfigContainer extends FabricConfigContainer {
     public CommonFabricConfigContainer(CommonConfigAPI.ConfigType type, Consumer<ConfigBuilder> init, Runnable onLoad) {
         super(type, init);
         this.builder = new LazyLoadedValue<>(() -> new ConfigBuilderFabric(this.getConfigName(), type, init, onLoad));
+        ClassLoadHacks.runIf(Platform.getEnvironment() == Env.CLIENT, () -> () -> ClientPlayConnectionEvents.INIT.register((phase, handler) -> {
+            this.createOrLoad(null);
+            builder.get().onLoad(null);
+        }));
         LifecycleEvent.SERVER_BEFORE_START.register(state -> {
             this.createOrLoad(null);
             builder.get().onLoad(state);
